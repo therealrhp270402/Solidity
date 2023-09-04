02c3f7bb82d9757a23a6b7bc07740fbd88b46737
RHP <114064840+therealrhp270402@users.noreply.github.com>
2023-09-04 17:34:29 +0700

    Create Default_Values.sol

---
+++ Default_Values.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract DefaultValues {
+    bool public a; // false
+    uint public b; // 0
+    int public c; // 0
+    address public d; // 0 address
+    bytes public e; // 0 bytes (64)
+}
