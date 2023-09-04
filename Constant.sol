d52b84862c683c33b7cf446c659532297e6114ac
RHP <114064840+therealrhp270402@users.noreply.github.com>
2023-09-04 16:43:46 +0700

    Create Constant.sol

---
+++ Constant.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract Constants {
+    // gas fee
+    uint public myUint = 123;
+
+    // gas fee of constant
+    uint public constant MY_UINT = 123;
+}
